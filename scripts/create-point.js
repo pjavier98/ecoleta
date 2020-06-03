16641ab47c47795dd63248d613eb5a29ed3878c9
Pedro Javier <pjpc@ic.ufal.br>
2020-06-03 15:37:58 -0300

    Next Level Week 03: Finish collection points, modal to search collection points

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -19,11 +19,14 @@ function getCities(event) {
   const ufSelectedIndex = event.target.selectedIndex;
   stateInput.value = event.target.options[ufSelectedIndex].text;
 
+  citiesSelect.innerHTML = '<option value>Selecione a Cidade</option>'
+  citiesSelect.disabled = true
+
   fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufId}/municipios`)
   .then(res => res.json())
   .then(cities => {
     cities.forEach(city => {
-      citiesSelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`;
+      citiesSelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`;
     });
     citiesSelect.disabled = false;
   });
@@ -34,3 +37,33 @@ document
   .addEventListener("change", getCities)
 
   populateUFs()
+
+
+// Itens de Coleta
+// pegar todos os li's
+const itemsToCollect = document.querySelectorAll(".items-grid li");
+
+itemsToCollect.forEach(item => {
+  item.addEventListener("click", handleSelectedItem);
+});
+
+let selectedItems = [];
+const collectedItems = document.querySelector('input[name=items]')
+
+function handleSelectedItem(event) {
+  const item = event.target;
+
+  item.classList.toggle("selected")
+  
+  const itemId = item.dataset.id;
+
+  const alreadySelected = selectedItems.findIndex(item => item == itemId);
+
+  if (alreadySelected < 0) {
+    selectedItems.push(itemId);
+  } else {
+    selectedItems = selectedItems.filter(item => item != itemId);
+  }
+
+  collectedItems.value = selectedItems;
+}
